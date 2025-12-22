a87af9de4761c7ef75beedec10eb5e834a98f89d
root <badrsha321@gmail.com>
2025-12-22 14:10:00 +0000

    adding new functions in the simple file to make the shell do ls command

--- simple.c
+++ simple.c
@@ -1,22 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
 
-int main (void)
-{
-char *argv[] = {"/bin/ls", NULL};
+int main(void) {
+char *argv[] = {NULL, NULL};
+pid_t pid;
+char line[1024];
 
 while (1)
 {
 printf("cisfun$ ");
-size_t pid = fork();
-if (pid == 0)
-{
-execve(argv[0],argv ,NULL);
+
+fgets(line, sizeof(line), stdin);
+if (line == NULL)
+break;
+
+line[strlen(line) - 1] = '\0';
+
+pid = fork();
+if (pid == 0) {
+argv[0] = line;
+execve(argv[0], argv, NULL);
 }
 else
 {
 wait(NULL);
 }
 }
-
+return 0;
 }
