d625b266673db177c29331d7e92eb73f749088f3
root <hri7isd@gmail.com>
2025-12-22 15:25:47 +0000

    update-- simple.c

--- simple.c
+++ simple.c
@@ -4,24 +4,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-char *trim_line(char *str)
-{
-    char *start = str;
-    char *end;
-
-    while (*start == ' ' || *start == '\t')
-        start++;
-
-    if (*start == '\0')
-        return start;
-
-    end = start + strlen(start) - 1;
-    while (end > start && (*end == ' ' || *end == '\t'))
-        end--;
-
-    *(end + 1) = '\0';
-    return start;
-}
+char *trim(char *str);
 
 int main(void)
 {
@@ -31,7 +14,6 @@ int main(void)
     char *argv[2];
     pid_t pid;
     extern char **environ;
-    char *trimmed;
 
     while (1)
     {
@@ -44,31 +26,24 @@ int main(void)
         nread = getline(&line, &len, stdin);
         if (nread == -1)
             break;
-        if (nread == 1)
-            continue;
+
         if (line[nread - 1] == '\n')
             line[nread - 1] = '\0';
 
-        trimmed = trim_line(line);
-        if (*trimmed == '\0')
+        line = trim(line);
+
+        if (line[0] == '\0')
             continue;
 
         pid = fork();
-        if (pid == -1)
-        {
-            perror("fork failed");
-            continue;
-        }
-
         if (pid == 0)
         {
-            argv[0] = trimmed;
+            argv[0] = line;
             argv[1] = NULL;
-            if (execve(argv[0], argv, environ) == -1)
-            {
-                printf("./shell: No such file or directory\n");
-                exit(EXIT_FAILURE);
-            }
+
+            execve(argv[0], argv, environ);
+            printf("./shell: No such file or directory\n");
+            exit(EXIT_FAILURE);
         }
         else
         {
@@ -78,4 +53,17 @@ int main(void)
 
     free(line);
     return 0;
+}
+
+char *trim(char *str)
+{
+    char *end;
+
+    while (*str == ' ' || *str == '\t') str++;
+    if (*str == 0) return str;
+
+    end = str + strlen(str) - 1;
+    while (end > str && (*end == ' ' || *end == '\t')) end--;
+    *(end + 1) = '\0';
+    return str;
 }
