d92f3432ee6c3daf7e55e15837ffeed83b111409
root <hri7isd@gmail.com>
2025-12-22 15:08:17 +0000

    fix-- simple error

--- simple.c
+++ simple.c
@@ -25,13 +25,15 @@ int main(void)
             break;
 
 	if (nread == 1)
-	continue;
-
-        if (line[0] == '\n' || line[0] == ' ' || line[0] == '\t')
             continue;
 
         if (line[nread - 1] == '\n')
             line[nread - 1] = '\0';
+
+	line = trim(line);
+
+	if (line[0] == '\0')
+    	    continue;
 
         pid = fork();
         if (pid == -1)
@@ -61,3 +63,17 @@ int main(void)
     return 0;
 }
 
+
+
+char *trim(char *str)
+{
+    char *end;
+
+    while (*str == ' ' || *str == '\t') str++;
+    if (*str == 0) return str;
+
+    end = str + strlen(str) - 1;
+    while (end > str && (*end == ' ' || *end == '\t')) end--;
+    *(end + 1) = '\0';
+    return str;
+}
