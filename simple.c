10a3d94c669f4af4c5b9c89fbde702fee19c5fb9
JNBLA6 <badrsha321@gmail.com>
2025-12-22 19:33:39 +0300

    adding tokinsztion to make the line sepreated and to make it knowing the flags

--- simple.c
+++ simple.c
@@ -28,7 +28,7 @@ int main(int argc, char **argv, char **env)
     char *line = NULL;
     size_t len = 0;
     ssize_t nread;
-    char *args[2];
+    char *args[64];
     pid_t pid;
     char *trimmed;
     FILE *fp;
@@ -95,4 +95,80 @@ int main(int argc, char **argv, char **env)
 
     free(line);
     return 0;
+}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+char *trim_line(char *str)
+{
+    char *start = str;
+    char *end;
+    while (*start == ' ' || *start == '\t') start++;
+    if (*start == '\0') return start;
+    end = start + strlen(start) - 1;
+    while (end > start && (*end == ' ' || *end == '\t')) end--;
+    *(end + 1) = '\0';
+    return start;
+}
+
+int main(int argc, char **argv, char **env)
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    char *args[64];
+    pid_t pid;
+    char *token;
+    int i;
+    FILE *fp;
+
+    if (argc == 2)
+    {
+        fp = fopen(argv[1], "r");
+        if (fp == NULL) { perror(argv[0]); return (1); }
+    }
+    else fp = stdin;
+
+    while (1)
+    {
+        if (fp == stdin && isatty(STDIN_FILENO))
+        {
+            printf("cisfun$ ");
+            fflush(stdout);
+        }
+
+        nread = getline(&line, &len, fp);
+        if (nread == -1) break;
+
+        if (line[nread - 1] == '\n') line[nread - 1] = '\0';
+
+        i = 0;
+        token = strtok(line, " \t");
+        while (token != NULL && i < 63)
+        {
+            args[i++] = token;
+            token = strtok(NULL, " \t");
+        }
+        args[i] = NULL;
+
+        if (args[0] == NULL) continue;
+
+        pid = fork();
+        if (pid == 0)
+        {
+            if (execve(args[0], args, env) == -1)
+            {
+                fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+                exit(127);
+            }
+        }
+        else wait(NULL);
+    }
+
+    free(line);
+    if (fp != stdin) fclose(fp);
+    return (0);
 }
