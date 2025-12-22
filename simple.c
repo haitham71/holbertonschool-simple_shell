62146febbc6e20b76de83ee0c2337d9eb068318b
root <hri7isd@gmail.com>
2025-12-22 14:46:26 +0000

    fix-- simple error

--- simple.c
+++ simple.c
@@ -1,51 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/wait.h>
 
-/**
- * main - مفسر أوامر بسيط (Simple Shell)
- * يستخدم getline للقراءة و execve للتنفيذ
- */
 int main(void)
 {
-char *line = NULL;
-size_t len = 0;
-ssize_t nread;
-char *argv[] = {NULL, NULL};
-pid_t pid;
-while (1)
-{
-printf("cisfun$ ");
-fflush(stdout);
-nread = getline(&line, &len, stdin);
-if (nread == -1)
-break;
-if (nread == 1)
-continue;
-if (line[nread - 1] == '\n')
-line[nread - 1] = '\0';
-pid = fork();
-if (pid == -1)
-{
-perror("fork failed");
-continue;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    char *argv[2];
+    pid_t pid;
+    extern char **environ;
+
+    while (1)
+    {
+        printf("cisfun$ ");
+        fflush(stdout);
+
+        nread = getline(&line, &len, stdin);
+
+        if (nread == -1)
+            break;
+
+        if (nread == 1)
+            continue;
+
+        if (line[nread - 1] == '\n')
+            line[nread - 1] = '\0';
+
+        pid = fork();
+        if (pid == -1)
+        {
+            perror("fork failed");
+            continue;
+        }
+
+        if (pid == 0)
+        {
+            argv[0] = line;
+            argv[1] = NULL;
+
+            if (execve(argv[0], argv, environ) == -1)
+            {
+                printf("./shell: No such file or directory\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        else
+        {
+            wait(NULL);
+        }
+    }
+
+    free(line);
+    return 0;
 }
-if (pid == 0)
-{
-argv[0] = line;
-if (execve(argv[0], argv, NULL) == -1)
-{
-perror("./shell");
-exit(EXIT_FAILURE);
-}
-}
-else
-{
-wait(NULL);
-}
-}
-free(line);
-return (0);
-}
+
