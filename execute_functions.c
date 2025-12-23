68b8b73fcf71d5a0da8075ac9b10b31b465f2895
haitham71 <hri7isd@gmail.com>
2025-12-23 19:59:16 +0300

    add path func 1.2 + ubdate exe func

--- execute_functions.c
+++ execute_functions.c
@@ -1,49 +1,39 @@
 #include "shell_headers.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-#include <unistd.h>
-
 /**
- * execute_command - Creates a child process and executes the command
- * @args: Array of arguments (command and its options)
+ * execute_command - Executes a command if found
+ * @args: Array of arguments
  * @env: Environment variables
  */
 void execute_command(char **args, char **env)
 {
-    pid_t pid;
-    char *cmd_path;
+	pid_t pid;
+	char *cmd_path;
 
-    if (!args[0])
-        return;
+	cmd_path = find_command_path(args[0], env);
+	if (!cmd_path)
+	{
+		fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+		return;
+	}
 
-    cmd_path = find_command_path(args[0]);
-    if (!cmd_path)
-    {
-        fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-        return;
-    }
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		free(cmd_path);
+		return;
+	}
 
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("fork");
-        free(cmd_path);
-        return;
-    }
+	if (pid == 0)
+	{
+		if (execve(cmd_path, args, env) == -1)
+		{
+			perror("./hsh");
+			exit(127);
+		}
+	}
+	else
+		wait(NULL);
 
-    if (pid == 0)
-    {
-        if (execve(cmd_path, args, env) == -1)
-        {
-            fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-            free(cmd_path);
-            exit(127);
-        }
-    }
-    else
-    {
-        wait(NULL);
-        free(cmd_path);
-    }
+	free(cmd_path);
 }
--- path_functions.c
+++ path_functions.c
@@ -21,59 +21,33 @@ char *get_path_value(char **env)
 	return (NULL);
 }
 
-#include "shell_headers.h"
-#include <sys/stat.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
-
-/**
- * find_command_path - Searches for the command in the PATH
- * @command: The command name
- *
- * Return: Full path if found, NULL if not
- */
-char *find_command_path(char *command)
+char *find_command_path(char *command, char **env)
 {
-    char *path_env, *path_copy, *dir, *full_path;
-    struct stat st;
+	char *path, *path_copy, *dir;
+	char full_path[1024];
+	struct stat st;
 
-    /* Get PATH environment variable manually from environ */
-    extern char **environ;
-    int i = 0;
-    path_env = NULL;
-    while (environ[i])
-    {
-        if (strncmp(environ[i], "PATH=", 5) == 0)
-        {
-            path_env = environ[i] + 5;
-            break;
-        }
-        i++;
-    }
+	if (stat(command, &st) == 0)
+		return (strdup(command));
 
-    if (!path_env || path_env[0] == '\0')
-        return (NULL);
+	path = get_path_value(env);
+	if (!path)
+		return (NULL);
 
-    path_copy = strdup(path_env);
-    if (!path_copy)
-        return (NULL);
+	path_copy = strdup(path);
+	dir = strtok(path_copy, ":");
 
-    dir = strtok(path_copy, ":");
-    while (dir)
-    {
-        full_path = malloc(strlen(dir) + 1 + strlen(command) + 1);
-        if (!full_path)
-            return (NULL);
-        sprintf(full_path, "%s/%s", dir, command);
-        if (stat(full_path, &st) == 0)
-        {
-            free(path_copy);
-            return (full_path);
-        }
-        free(full_path);
-        dir = strtok(NULL, ":");
-    }
-    free(path_copy);
-    return (NULL);
+	while (dir)
+	{
+		sprintf(full_path, "%s/%s", dir, command);
+		if (stat(full_path, &st) == 0)
+		{
+			free(path_copy);
+			return (strdup(full_path));
+		}
+		dir = strtok(NULL, ":");
+	}
+
+	free(path_copy);
+	return (NULL);
 }
