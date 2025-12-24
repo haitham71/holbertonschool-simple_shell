b8d8dd15f354db3efef78f6c50143706dd1d45dc
JNBLA6 <badrsha321@gmail.com>
2025-12-24 15:56:44 +0300

    fix: streamline command path retrieval logic in find_command_path function

--- path_functions.c
+++ path_functions.c
@@ -1,38 +1,42 @@
 #include "shell_headers.h"
 
+/* Returns the full path of command if exists, else NULL */
 char *find_command_path(char *command, char **env)
 {
-    char *path_env, *path_copy, *dir, *full_path;
+    char *path_env = get_path_value(env);
+    char *path_copy, *dir;
     struct stat st;
+    char *full_path = NULL;
 
-    if (command[0] == '/' || command[0] == '.')
+    if (!path_env || command[0] == '/' || command[0] == '.')
     {
         if (stat(command, &st) == 0)
-            return (strdup(command));
+
+            return strdup(command);
         return (NULL);
     }
 
-    path_env = get_path_value(env);
-    if (!path_env || strlen(path_env) == 0)
-        return (NULL);
-
     path_copy = strdup(path_env);
+    if (!path_copy)
+        return NULL;
+
     dir = strtok(path_copy, ":");
     while (dir)
     {
         full_path = malloc(strlen(dir) + strlen(command) + 2);
-        if (!full_path) break;
-
+        if (!full_path)
+            break;
         sprintf(full_path, "%s/%s", dir, command);
+
         if (stat(full_path, &st) == 0)
         {
             free(path_copy);
-            return (full_path);
+            return full_path;
         }
         free(full_path);
+        full_path = NULL;
         dir = strtok(NULL, ":");
     }
-
     free(path_copy);
-    return (NULL);
+    return NULL;
 }
