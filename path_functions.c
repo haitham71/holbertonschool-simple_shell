51753ddf262d16fcfccfb8c4ad6c059ce07a2dc0
haitham71 <hri7isd@gmail.com>
2025-12-24 19:21:25 +0300

    review 0.2

--- path_functions.c
+++ path_functions.c
@@ -10,51 +10,51 @@
  */
 char *find_command_path(char *command, char **env)
 {
-    char *path_env, *path_copy, *dir, *full_path;
-    size_t len;
+	char *path_env, *path_copy, *dir, *full_path;
+	size_t len;
 
-    if (!command || *command == '\0')
-        return (NULL);
+	if (!command || *command == '\0')
+		return (NULL);
 
-    /* Case: command contains '/' */
-    if (strchr(command, '/') != NULL)
-    {
-        if (access(command, X_OK) == 0)
-            return (strdup(command));
-        return (NULL);
-    }
+	/* Case: command contains '/' */
+	if (strchr(command, '/') != NULL)
+	{
+		if (access(command, X_OK) == 0)
+			return (strdup(command));
+		return (NULL);
+	}
 
-    path_env = get_path_value(env);
-    if (!path_env || *path_env == '\0')
-        return (NULL);
+	path_env = get_path_value(env);
+	if (!path_env || *path_env == '\0')
+		path_env = "/bin:/usr/bin";
 
-    path_copy = strdup(path_env);
-    if (!path_copy)
-        return (NULL);
+	path_copy = strdup(path_env);
+	if (!path_copy)
+		return (NULL);
 
-    dir = strtok(path_copy, ":");
-    while (dir)
-    {
-        len = strlen(dir) + strlen(command) + 2;
-        full_path = malloc(len);
-        if (!full_path)
-        {
-            free(path_copy);
-            return (NULL);
-        }
+	dir = strtok(path_copy, ":");
+	while (dir)
+	{
+		len = strlen(dir) + strlen(command) + 2;
+		full_path = malloc(len);
+		if (!full_path)
+		{
+			free(path_copy);
+			return (NULL);
+		}
 
-        snprintf(full_path, len, "%s/%s", dir, command);
+		snprintf(full_path, len, "%s/%s", dir, command);
 
-        if (access(full_path, X_OK) == 0)
-        {
-            free(path_copy);
-            return (full_path);
-        }
+		if (access(full_path, X_OK) == 0)
+		{
+			free(path_copy);
+			return (full_path);
+		}
 
-        free(full_path);
-        dir = strtok(NULL, ":");
-    }
+		free(full_path);
+		dir = strtok(NULL, ":");
+	}
 
-    free(path_copy);
-    return (NULL);
+	free(path_copy);
+	return (NULL);
 }
